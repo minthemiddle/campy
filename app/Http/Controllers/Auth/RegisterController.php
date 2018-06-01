89c2657f0d5c3cb68dc2303ac78c3b446de9ade3
Martin Betz <martinmichaelbetz@gmail.com>
2018-06-01 17:54:23 +0200

    Add: Minimum registration age 9

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -47,11 +47,13 @@ class RegisterController extends Controller
      */
     protected function validator(array $data)
     {
+        $minimumAge = Carbon::now()->subYears(9);
+
         return Validator::make($data, [
             'username' => 'required|string|max:255|unique:users',
             'email' => 'required|string|email|max:255',
             'password' => 'required|string|min:6|confirmed',
-            'birthdate' => 'required|date',
+            'birthdate' => 'required|date|before:$minimumAge',
         ]);
     }
 
