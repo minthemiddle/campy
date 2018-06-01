5718a9299a7909f441614dc3b3a5fb688d42cb3e
Martin Betz <martinmichaelbetz@gmail.com>
2018-06-01 08:43:45 +0200

    Fix: Missing Str helper

--- app/Http/Controllers/CampUserController.php
+++ app/Http/Controllers/CampUserController.php
@@ -8,6 +8,7 @@ use App\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
 
 class CampUserController extends Controller
 {
