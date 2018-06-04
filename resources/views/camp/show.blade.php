ace354c3da70b0d0daadf5d23e360493e24905c9
Martin Betz <martinmichaelbetz@gmail.com>
2018-06-04 14:17:17 +0200

    Fix: Responsive tables

--- resources/views/camp/show.blade.php
+++ resources/views/camp/show.blade.php
@@ -6,27 +6,27 @@
         <p class="text-sm">Alle Camps anzeigen, aktuell auch noch jene, für die ich schon angemeldet bin.</p>
     </div>
 
-    <table class="table">
-  <thead>
-    <tr>
-      <th scope="col">Stadt</th>
-      <th scope="col">Von</th>
-      <th scope="col">Bis</th>
-      <th scope="col">Status</th>
-      <th scope="col">Optionen</th>
-    </tr>
-  </thead>
-  <tbody>
-    @foreach ($camps as $camp)
-    <tr>
-      <td class="font-bold">{{ $camp->city }}</th>
-      <td>{{ $camp->from->format('d.m.Y') }}</td>
-      <td>{{ $camp->to->format('d.m.Y') }}</td>
-      <td><span class="rounded p-2 @if ( $camp->status == 'Warteliste' ) bg-orange-light @else bg-green text-white @endif">{{ $camp->status }}</span></td>
-      <td>@if ($user->complete == '1')<a href="mycamps/create/{{ $camp->id  }}">Anmelden</a>@else <a href="/home">Profil vervollständigen</a>@endif</td>
-    </tr>
-
-    @endforeach
-  </tbody>
-</table>
+    <div class="table-responsive"><table class="table">
+      <thead>
+        <tr>
+          <th scope="col">Stadt</th>
+          <th scope="col">Von</th>
+          <th scope="col">Bis</th>
+          <th scope="col">Status</th>
+          <th scope="col">Optionen</th>
+        </tr>
+      </thead>
+      <tbody>
+        @foreach ($camps as $camp)
+        <tr>
+          <td class="font-bold"><a href="mycamps/create/{{ $camp->id  }}">{{ $camp->city }}</a></th>
+          <td>{{ $camp->from->format('d.m.Y') }}</td>
+          <td>{{ $camp->to->format('d.m.Y') }}</td>
+          <td><p class="rounded p-2 @if ( $camp->status == 'Warteliste' ) bg-orange-light @else bg-green text-white @endif">{{ $camp->status }}</p></td>
+          <td>@if ($user->complete == '1')<a href="mycamps/create/{{ $camp->id  }}">Anmelden</a>@else <a href="/home">Profil vervollständigen</a>@endif</td>
+        </tr>
+    
+        @endforeach
+      </tbody>
+    </table></div>
 @endsection
