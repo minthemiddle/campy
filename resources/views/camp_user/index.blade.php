f39789d229a7eae7932e2499dc7e30d58ca942e5
Martin Betz <martinmichaelbetz@gmail.com>
2018-06-04 14:03:56 +0200

    Update: Registration detail section

--- resources/views/camp_user/index.blade.php
+++ resources/views/camp_user/index.blade.php
@@ -4,6 +4,7 @@
 <div class="mb-4">
     <div class="mb-4"><h2>Meine Camps</h2>
         <p class="text-sm">Nur die Camps anzeigen, für die ich registriert bin.</p>
+        <p class="text-sm mt-2">Über den Link <strong>Details</strong> kannst du bei jedem Camp den Fortschritt deiner Anmeldung sehen (Laptop, Bezahlung), innerhalb der Frist deine Daten ändern und absagen.</p>
     </div>
 
 <table class="table">
@@ -20,12 +21,12 @@
   <tbody>
     @foreach ($camp_user as $camp)
     <tr>
-      <td class="font-bold"><a href="mycamps/{{ $camp->id }}">{{ $camp->city }}</a></th>
+      <td class="font-bold"><a href="mycamps/{{ $camp->id }}/edit">{{ $camp->city }}</a></th>
       <td>{{ $camp->from->format('d.m.Y') }}</td>
       <td>{{ $camp->to->format('d.m.Y') }}</td>
       <td>@component('components.status_camp', ['camp' => $camp->pivot->status]) @endcomponent</td>
       <td>@component('components.status_laptop', ['laptop' => $camp->pivot->laptop]) @endcomponent</td>
-      <td><a href="mycamps/{{ $camp->id }}/edit">Ändern/Absagen</a></td>
+      <td><a href="mycamps/{{ $camp->id }}/edit">Details</a></td>
     </tr>
     @endforeach
   </tbody>
--- resources/views/camp_user/show.blade.php
+++ resources/views/camp_user/show.blade.php
@@ -3,16 +3,63 @@
 @section('content')
 
 <div class="ml-4 mb-4">
-<h2>Camp Anmeldung ändern</h2>
+<h2>Deine Camp-Teilnahme</h2>
 
 @isset($camp)
 {{ $camp->city }}, {{ $camp->from->format('d.m.Y') }} bis {{ $camp->to->format('d.m.Y') }}, {{$camp->shortcode }}
 @endisset
 
-<div class="mt-2">Mein Campstatus:
+<div class="mt-4 -ml-4 bg-white rounded-lg p-2 lg:w-2/3"><a href="#status">Status</a> | <a href="#data">Personendaten</a> | <a href="#reqs">Laptop/Beitrag</a> | <a href="#cancel">Absage</a></div>
 
-@component('components.status_camp', ['camp' => $camp->pivot->status]) @endcomponent
+<div class="-ml-4 p-4 mt-2 bg-grey-lighter lg:w-2/3 shadow" id="status"><h3 class="mb-6">Anmeldestatus</h3>
+
+<div class="mb-4">@component('components.status_camp', ['camp' => $camp->pivot->status]) @endcomponent</div>
+
+@if ($camp->pivot->status == 'registered' and $camp->pivot->contribution == 'payer')
+
+<p>Nächster Schritt: Um deine Anmeldung zu bestätigen, tätige bitte folgende Überweisung(en) bis zum <em>{{ $camp->from->subMonth(1)->format('d.m.Y') }}</em> wie folgt.</p>
+<p class="text-sm">Bitte: Unbedingt den Verwendungszweck 1:1 kopieren. Abweichungen verursachen extrem viel unnötige manuelle Arbeit.</p>
+
+
+<h4 class="mt-4">Unkostenbeitrag</h4>
+<ul class="leading-normal mb-4">
+  <li>Empfänger: Code+Design Initiative e.V.</li>
+  <li><em>IBAN:</em> DE24100500000190607629</li>
+  <li>BIC: <em>BELADEBEXXX</em></li>
+  <li>Betrag: {{ $camp->contribution }}€</li>
+  <li><em>Verwendungszweck:</em> Unkostenbeitrag {{ $user->firstname }} {{ $user->lastname }} {{ $camp->shortcode }}</li>
+</ul>
+
+@if ($camp->pivot->laptop == 'payer')
+<h4 class="mt-4">Laptop</h4>
+<ul class="leading-normal mb-4">
+  <li>Empfänger: Code+Design Initiative e.V.</li>
+  <li><em>IBAN:</em> DE24100500000190607629</li>
+  <li>BIC: <em>BELADEBEXXX</em></li>
+  <li>Betrag: {{ $camp->laptop }}</li>
+  <li><em>Verwendungszweck:</em> Laptop Unkostenbeitrag {{ $user->firstname }} {{ $user->lastname }} {{ $camp->shortcode }}</li>
+</ul>
+@endif
+
+
+@elseif ($camp->pivot->status == 'registered' and $camp->pivot->laptop == 'payer')
+
+<p>Nächster Schritt: Um deine Anmeldung zu bestätigen, tätige bitte folgende Überweisung bis zum <em>{{ $camp->from->subMonth(1)->format('d.m.Y') }}</em> wie folgt.</p>
+<p class="text-sm">Bitte: Unbedingt den Verwendungszweck 1:1 kopieren. Abweichungen verursachen extrem viel unnötige manuelle Arbeit.</p>
+
+<h4 class="mt-4">Laptop</h4>
+<ul class="leading-normal mb-4">
+  <li>Empfänger: Code+Design Initiative e.V.</li>
+  <li><em>IBAN:</em> DE24100500000190607629</li>
+  <li>BIC: <em>BELADEBEXXX</em></li>
+  <li>Betrag: {{ $camp->laptop }}</li>
+  <li><em>Verwendungszweck:</em> Laptop Unkostenbeitrag {{ $user->firstname }} {{ $user->lastname }} {{ $camp->shortcode }}</li>
+</ul>
+@endif
+
 </div>
+
+
 
 </div>
 
@@ -26,7 +73,7 @@
     {{ csrf_field() }}
     {{ method_field('PATCH') }}
 
-    <div class="bg-grey-lighter shadow p-4 rounded lg:w-2/3">
+    <div class="bg-grey-lighter shadow p-4 rounded lg:w-2/3" id="data">
         <p class="font-bold mb-4">Diese Daten von dir kennen wir schon…</p>
         <div class="flex">
           <div class="w-1/2">
@@ -53,41 +100,44 @@
         <p class="mt-4">Fehler entdeckt? Bitte <a href="/home" target="_blank">hier</a> korrigieren!</p>
     </div>
 
-    <h3 class="mt-8">Anmelde-Daten</h3>
-
+    <div class="w-2/3 bg-grey-lighter p-4 mt-8" id="reqs"><h3 class="mt-8">Laptop/Unkostenbeitrag</h3>
+    
+        <div class="mt-2">
+                    <label for="contribution">Unkostenbeitrag</label><br>
+                    <div class="relative">
+                      <select name="contribution" class="mt-2 block appearance-none w-full bg-white border border-grey-lighter text-grey-darker py-3 px-4 pr-8 rounded">
+                        <option value="payer" @if($camp->pivot->contribution == 'payer')selected @endif>Ich zahle 75€ Unkostenbeitrag</option>
+                        <option value="waiver" @if($camp->pivot->contribution == 'waiver')selected @endif>Ich kann den Unkostenbeitrag gerade nicht zahlen</option>
+                    </select>
+                    <div class="pointer-events-none absolute pin-y pin-r flex items-center px-2 text-grey-darker"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" class="fill-current h-4 w-4"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"></path></svg></div>
+                    </div>
+                    
+                </div>
+    
     <div class="mt-2">
-                <label for="contribution">Unkostenbeitrag</label><br>
+                <label for="laptop">Laptop</label><br>
                 <div class="relative">
-                  <select name="contribution" class="mt-2 block appearance-none w-full bg-white border border-grey-lighter text-grey-darker py-3 px-4 pr-8 rounded">
-                    <option value="payer" @if($camp->pivot->contribution == 'payer')selected @endif>Ich zahle 75€ Unkostenbeitrag</option>
-                    <option value="waiver" @if($camp->pivot->contribution == 'waiver')selected @endif>Ich kann den Unkostenbeitrag gerade nicht zahlen</option>
+                  <select name="laptop" class="mt-2 block appearance-none w-full bg-white border border-grey-lighter text-grey-darker py-3 px-4 pr-8 rounded">
+                     <option value="own" @if ($camp->pivot->laptop == 'own') selected @endif>Nein, ich nutze eigenen Laptop</option>
+                    <option value="payer" @if ($camp->pivot->laptop == 'payer' or $camp->pivot->laptop == 'paid') selected @endif>Ja, ich leihe Laptop und überweise 75€ Unkostenbeitrag</option>
+                    <option value="waiver" @if ($camp->pivot->laptop == 'waiver') selected @endif>Ja, ich leihe Laptop, kann aber den Unkostenbeitrag nicht zahlen</option>
+                    
                 </select>
                 <div class="pointer-events-none absolute pin-y pin-r flex items-center px-2 text-grey-darker"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" class="fill-current h-4 w-4"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"></path></svg></div>
                 </div>
-                
+    
+                <div class="mt-4"><label for="comment">Sonstiges</label><br>
+                            <textarea name="comment" id="" rows="10" class="w-full mt-2 p-2" placeholder="Alles, was sonst noch für deine Anmeldung wichtig ist: Allergien, Besonderheiten, sonstige Kommentare">{{ $camp->pivot->comment }}</textarea></div>
             </div>
-
-<div class="mt-2">
-            <label for="laptop">Laptop</label><br>
-            <div class="relative">
-              <select name="laptop" class="mt-2 block appearance-none w-full bg-white border border-grey-lighter text-grey-darker py-3 px-4 pr-8 rounded">
-                 <option value="own" @if ($camp->pivot->laptop == 'own') selected @endif>Nein, ich nutze eigenen Laptop</option>
-                <option value="payer" @if ($camp->pivot->laptop == 'payer' or $camp->pivot->laptop == 'paid') selected @endif>Ja, ich leihe Laptop und überweise 75€ Unkostenbeitrag</option>
-                <option value="waiver" @if ($camp->pivot->laptop == 'waiver') selected @endif>Ja, ich leihe Laptop, kann aber den Unkostenbeitrag nicht zahlen</option>
-                
-            </select>
-            <div class="pointer-events-none absolute pin-y pin-r flex items-center px-2 text-grey-darker"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" class="fill-current h-4 w-4"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"></path></svg></div>
-            </div>
-
-            <div class="mt-4"><label for="comment">Sonstiges</label><br>
-                        <textarea name="comment" id="" rows="10" class="w-full mt-2 p-2" placeholder="Alles, was sonst noch für deine Anmeldung wichtig ist: Allergien, Besonderheiten, sonstige Kommentare">{{ $camp->pivot->comment }}</textarea></div>
+          </div>
+    <div class="bg-grey-lighter mt-8 p-4 rounded-lg shadow lg:w-2/3">
+      <h3 id="cancel">Absage</h3>
+        <input type="hidden" name="camp" value="{{ $camp->id }}">
+        <input type="hidden" name="user" value="{{ $user->id }}">
+        <div><input type="checkbox" name="cancel" class="mt-8"> <span class="text-red">Ich muss leider absagen, weil…</span><br>
+            <textarea name="reason_for_cancellation" id="" rows="10" class="w-full mt-2 p-2"></textarea></div>
         </div>
-
-    <input type="hidden" name="camp" value="{{ $camp->id }}">
-    <input type="hidden" name="user" value="{{ $user->id }}">
-    <div>@{{ message }}<input type="checkbox" name="cancel" class="mt-8"> <span class="text-red">Ich muss leider absagen, weil…</span><br>
-        <textarea name="reason_for_cancellation" id="" rows="10" class="w-full mt-2 p-2"></textarea></div>
-    <input type="submit" value="Ändern/Absagen" class="mt-4 bg-blue p-2 text-white shadow">
+        <input type="submit" value="Ändern/Absagen" class="mt-4 bg-blue p-2 text-white shadow">
 </form>
 
       </div>
