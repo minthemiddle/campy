7566d5f98dec3d1af0479b7c0f859611f80c41c5
Martin Betz <martinmichaelbetz@gmail.com>
2018-06-08 15:03:37 +0200

    Remove: Unnecessary php variables

--- resources/views/admin/show.blade.php
+++ resources/views/admin/show.blade.php
@@ -24,10 +24,6 @@
       <td>{{ $camp->to->format('d.m.y') }}</td>
       <td>{{ $camp->freeSpots }}</td>
       <td>
-        @php
-          $subject = urlencode('Code+Design');
-          $salutation = urlencode('Hallo');
-        @endphp
         <table class="overflow-x-auto">
           <thead>
             <tr>
@@ -44,7 +40,7 @@
             <tr>
             <td>{{ $user->firstname }}</td>
             <td>{{ $user->lastname }}</td>
-            <td><a href="mailto:{{ $user->email }}?subject={{ $subject }}%20{{ $camp->city }}&body={{ $salutation }}%20{{ $user->firstname }}">{{ $user->email }}</a></td>
+            <td><a href="mailto:{{ $user->email }}?subject=Code+Design%20{{ $camp->city }}&body=Hallo%20{{ $user->firstname }}">{{ $user->email }}</a></td>
             <td>{{ $user->pivot->status }}</td>
             <td>{{ $user->pivot->laptop }}</td>
             <td>{{ $user->pivot->contribution }}</td>
