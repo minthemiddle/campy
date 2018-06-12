f0432b8928f1c14ac1f853ceff607b7744dababc
Martin Betz <martinmichaelbetz@gmail.com>
2018-06-12 08:49:32 +0200

    Add: Test user profile complete

---
+++ tests/Browser/CampRegistrationTest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Tests\Browser;
+
+use App\Camp;
+use App\User;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Laravel\Dusk\Browser;
+use Tests\DuskTestCase;
+
+class CampRegistrationTest extends DuskTestCase
+{
+    use DatabaseMigrations;
+
+    /** @test */
+    public function users_can_not_register_with_incomplete_profile()
+    {
+
+        $user = factory(User::class)->create();
+        $camp = factory(Camp::class)->create();
+
+
+
+        $this->browse(function (Browser $browser) {
+            $browser->loginAs(User::find(1))
+                    ->visit('/camps')
+                    ->assertSee('Profil vervollständigen');
+        });
+    }
+}
