ebf1f3a120afa4d671281c3d8e0723f37e510072
Martin Betz <martinmichaelbetz@gmail.com>
2018-07-21 17:56:22 +0200

    Fix: Double semi colon

--- tests/Unit/UserControllerTest.php
+++ tests/Unit/UserControllerTest.php
@@ -12,7 +12,7 @@ class UserControllerTest extends TestCase
     /** @test */
     public function a_user_with_incomplete_profile_cannot_sign_up_for_a_camp()
     {    
-        $userA = factory(User::class)->make();;
+        $userA = factory(User::class)->make();
         $response = $this->get('/camps');
         $response->assertSee('fertigstellen');
     }
