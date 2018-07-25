47c6ab1a767cbb904427bb477df13daed3ad3ec2
Martin Betz <martinmichaelbetz@gmail.com>
2018-07-25 11:40:54 +0200

    Fix: Show Registered Users per camp

--- resources/views/admin/show.blade.php
+++ resources/views/admin/show.blade.php
@@ -15,7 +15,7 @@
     <thead>
         <tr>
             <td class="p-2 border border-black border-2 bg-grey-lighter">
-              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-users"><title>Registriert</title><path d="M17 21v-2a4 4 0 0 0-4-4H5a4 4 0 0 0-4 4v2"></path><circle cx="9" cy="7" r="4"></circle><path d="M23 21v-2a4 4 0 0 0-3-3.87"></path><path d="M16 3.13a4 4 0 0 1 0 7.75"></path></svg>
+              <div>R</div>
             </td>
             <td class="p-2 border border-black border-2 bg-grey-lighter">
               <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-square"><title>Frei</title><rect x="3" y="3" width="18" height="18" rx="2" ry="2"></rect></svg>
@@ -30,7 +30,7 @@
     </thead>
     <tbody>
         <tr>
-            <td class="p-2 border border-black border-2">{{ $camp->totalParticipants }}</td>
+            <td class="p-2 border border-black border-2">{{ $camp->RegisteredParticipants }}</td>
             <td class="p-2 border border-black border-2">{{ $camp->freeSpots }}</td>
             <td class="p-2 border border-black border-2">{{ $camp->totalGirls }}</td>
             <td class="p-2 border border-black border-2">{{ $camp->orderedlaptops }}</td>
