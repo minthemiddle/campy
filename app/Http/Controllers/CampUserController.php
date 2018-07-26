cc8b6dfaaccf8faa6afd82b897c6cde5a06c81f7
Martin Betz <martinmichaelbetz@gmail.com>
2018-07-26 22:54:46 +0200

    Add: Use relevant packages

--- app/Http/Controllers/CampUserController.php
+++ app/Http/Controllers/CampUserController.php
@@ -4,11 +4,13 @@ namespace App\Http\Controllers;
 
 use App\Camp;
 use App\CampUser;
+use App\Mail\ContributionConfirmed;
 use App\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\App;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Session;
 use Illuminate\Support\Str;
 
