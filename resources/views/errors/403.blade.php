5dffd234962cf1647fb6579a74e6d97a0323905b
Julius <jubeki99@gmail.com>
2018-08-07 09:45:59 +0200

    Add: error pages

---
+++ resources/views/errors/403.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.app')
+
+@section('content')
+    <h1>Du besitzt die Berechtigungen nicht.</h1>
+    <p>Du denkst dies ist ein Fehler? Dann schreib den Fehler <a href="https://github.com/CodeDesignInitiative/campy/issues">hier</a></p>
+@endsection
