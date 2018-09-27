caa1489d682d07dc4cf82208005c043c3b821749
Martin Betz <martinmichaelbetz@gmail.com>
2018-09-27 19:25:40 +0200

    Cut: Links on city names

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -78,7 +78,7 @@
       <tbody>
         @foreach ($camps as $camp)
         <tr>
-          <td class="font-bold"><a href="mycamps/create/{{ $camp->id  }}">{{ $camp->city }}</a></th>
+          <td class="font-bold"><p class="font-bold">{{ $camp->city }}</p></th>
           <td>{{ $camp->from->format('d.m.') }}</td>
           <td>{{ $camp->to->format('d.m.Y') }}</td>
           <td class=""><p class="flex items-center"><span class="mr-2 inline-block rounded-full w-3 h-3 @if ( $camp->status == 'Warteliste' ) bg-orange-light @else bg-green text-white @endif">&zwnj;</span>{{ $camp->status }}</p></td>
